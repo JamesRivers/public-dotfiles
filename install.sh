94035dca13a2b6fa248454c1260e6ce3218d011d
James Rivers <rivers1980@gmail.com>
2023-10-22 20:19:41 +0100

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install tmux
+sudo apt install neovim 
+sudo apt install gh
+sudo apt install git
+sudo apt install htop
+sudo apt install lynx
+
+##SNAPS
+sudo snap install newsboat
